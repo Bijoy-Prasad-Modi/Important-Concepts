c6c23bc0cb115ba0693aaee48078dea29fa8ad9e
Bijoy <bpmodi88@gmail.com>
2023-08-05 01:17:27 +0530

    New concept asynchronus-await file being added

---
+++ JavaScript/Asynchronus/05-asyncAwait.js
@@ -0,0 +1,41 @@
+// In this case we will will create and icecream ordering system using async await
+//
+
+let stocks = {
+    fruits: ["strawberry", "mango", "watermelon", "apple", "grapes"],
+    liquid: ["water", "ice"],
+    holder: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
+  };
+  let is_shop_open = true;
+  
+  let toppings_choice =()=>{ //Using this function to ask customer, what toppings they would like to add to the icecream
+  
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        resolve(console.log("Which topping do you want!"));
+      },3000)
+    });
+  };
+  
+  //In the kitchen function the preparatio of the icecream would be done
+  async function kitchen () {
+  console.log(" A ")
+    console.log(" B ")
+    console.log(" C ")
+  
+    //while we are working on the kitchen we realise we forget to ask customer what toppings they would like. So we are calling the await function
+    await toppings_choice() //Using await toppings_choice() function we are going outside of the kitchen fucntion to ask customer toppings of their choices
+    
+    console.log(" D ")
+    console.log(" E  ")
+    
+  }
+  
+  kitchen()
+  
+
+  //while, in the kitchen icecream preparation going on or not this👇 other task would be keep on going
+  console.log("doing the dishes")
+  console.log("cleaning the table")
+  console.log("taking others orders")
